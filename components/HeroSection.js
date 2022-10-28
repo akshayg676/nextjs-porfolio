18e1c568c67436a8bfad3f17b2f44e7d44071770
AkshayG-accruon <akshay@accruontechnologies.co.in>
2022-10-28 23:19:10 +0530

    complete hero section

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,7 +1,28 @@
 import React from "react";
-
+import styles from "../styles/HeroSection.module.css";
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div>
+      {/* -------------------------------------- header -------------------------------------- */}
+      <div className={styles.header}>
+        <div className={styles.headerContainer}>
+          <h1>
+            Akshay G<br />
+            Full Stack
+            <br />
+            &nbsp;Developer
+          </h1>
+        </div>
+      </div>
+
+      {/* -------------------------------------- hero-image -------------------------------------- */}
+      <div className={styles.heroImage}>
+        <div className={styles.heroImageContainer}>
+          <img src="/images/stripy-relax.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
